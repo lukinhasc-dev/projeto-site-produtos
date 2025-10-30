652723ae1163bdeba9d4b3437fc8497f135f4aa3
vt-debug <pimpolhoplays8@gmail.com>
2025-10-30 20:52:07 -0300

    config bd

--- backend/src/models/connectionModel.ts
+++ backend/src/models/connectionModel.ts
@@ -1,4 +1,11 @@
 import mysql from 'mysql2/promise'
 import { config } from 'dotenv'
+config()
 
-export const connectModule = mysql.createPool({})
+export const connectionModule = mysql.createPool({
+    host:process.env.DB_HOST || '',
+    user:process.env.DB_USER || '',
+    password:process.env.DB_PASSWORD || '',
+    database:process.env.DB_DATABASE || '',
+
+})
---
+++ backend/src/models/produtoModel.ts
@@ -0,0 +1,18 @@
+import { connectionModule } from "./connectionModel";
+
+    const getAllProduct =  async () => {}
+    const getByIDProduct = async () => {}
+    const creatnewProduct = async () => {}
+    const editPartialProduct = async () => {}
+    const removeProduct = async () => {}
+
+
+
+export default {
+    getAllProduct,
+    getByIDProduct,
+    creatnewProduct,
+    editPartialProduct,
+    removeProduct
+
+}
