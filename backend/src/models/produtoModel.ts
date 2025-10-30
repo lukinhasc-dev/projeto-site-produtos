525ebb9f56b88532625f56200ddfd226d77f9f33
vt-debug <pimpolhoplays8@gmail.com>
2025-10-30 20:58:05 -0300

    c

--- backend/src/models/produtoModel.ts
+++ backend/src/models/produtoModel.ts
@@ -1,7 +1,13 @@
 import { connectionModule } from "./connectionModel";
 
-    const getAllProduct =  async () => {}
-    const getByIDProduct = async () => {}
+    const getAllProduct =  async () => {
+        const [listProduct] = await connectionModule.execute('SELECT * FROM product')
+        return listProduct
+    }
+    const getByIDProduct = async (id:number) => {
+            const [Product] = await connectionModule.execute(`SELECT * FROM product where id=${id}`)
+        return Product
+    }
     const creatnewProduct = async () => {}
     const editPartialProduct = async () => {}
     const removeProduct = async () => {}
