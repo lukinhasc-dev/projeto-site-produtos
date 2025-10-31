48dd7afbf2c9142057b8cefbe431cc55864094a0
vt-debug <pimpolhoplays8@gmail.com>
2025-10-30 21:37:03 -0300

    .

--- backend/src/controller/produtoController.ts
+++ backend/src/controller/produtoController.ts
@@ -1,21 +1,22 @@
+import { Request, Response } from "express";
 import { IProduct } from "../interfaces/types";
 import produtoModel from "../models/produtoModel";
 
-    const getAllProduct =  async () => {
-        const [listProduct] = await produtoModel.getAllProduct('SELECT * FROM product')
-        return listProduct
+    const getAllProduct =  async (req:Request, res:Response) => {
+        const listProduct = await produtoModel.getAllProduct()
+        return res.status(200).json (listProduct)
     }
-    const getByIDProduct = async (id:number) => {
-            const [Product] = await produtoModel.getByIDProduct(`SELECT * FROM product where id=${id}`)
+    const getByIDProduct = async (req:Request, res:Response) => {
+            const Product = await produtoModel.getByIDProduct(Number(req.params.id))
         return Product
     }
-    const creatnewProduct = async (body:IProduct) => {
+    const creatnewProduct = async (req:Request, res:Response) => {
         const {name,description,price,stock,createdAt,updateAt} = body
         const query = 'INSERT INTO product(name,description,price,stock,createdAt,updateAt) values(?,?,?,?,?,?)'
-        const [newProduct] = await produtoModel.creatnewProduct(query,[name,description,price,stock,createdAt ?? new Date(),updateAt ?? Date()])
+        const newProduct = await produtoModel.creatnewProduct(query,[name,description,price,stock,createdAt ?? new Date(),updateAt ?? Date()])
         return newProduct
     }
-    const editPartialProduct = async (id:number, update: Partial<IProduct>) => {
+    const editPartialProduct = async (req:Request, res:Response) => {
 
         delete update.createdAt;
         if(!update.updateAt){
@@ -26,11 +27,11 @@ import produtoModel from "../models/produtoModel";
         const values = Object.values(update)
         const setclause = fields.map(field => `${field}=?`).join(',')
         const query = `UPDATE product set ${setclause}, updateAt= NOW() WHERE id=?`
-        const [editProduct] = await produtoModel.editPartialProduct(query,[...values, id])
+        const editProduct = await produtoModel.editPartialProduct(query,[...values, id])
         return editProduct
 
     }
-    const removeProduct = async (id:number) => {
+    const removeProduct = async (req:Request, res:Response) => {
         const deleteProduct = await produtoModel.removeProduct(`DELETE FROM product WHERE id=${id}`)
         return deleteProduct
     }
